a968184e80ddf3c3a14d53c31ced5d7b08c57d49
issuehwa <issuehwa@192.168.200.183>
2020-10-20 17:56:24 +0900

    기능추가: 빈문자열 체크, 원화로 표기
    StringUtil에 추가
    사용자 입력값 중 공백이 있는지 체크(결제시)
    원화로 표기 하는 기능

--- src/main/java/net/class101/homework1/common/util/StringUtil.java
+++ src/main/java/net/class101/homework1/common/util/StringUtil.java
@@ -1,5 +1,7 @@
 package net.class101.homework1.common.util;
 
+import java.text.DecimalFormat;
+
 public class StringUtil {
     
     //문자열 숫자 구분
@@ -10,5 +12,20 @@ public class StringUtil {
         } catch (NumberFormatException e) {
             return false;
         }
+    }
+    
+    //문자열 공백 또는 빈문자열 체크
+    public static boolean isEmptyString(String str) {
+        return str == null || str.trim().length() == 0;
+    }
+    
+    //원화로 표기
+    public static String getDecimalKRW(int price) {
+        String resultKRW;
+        
+        DecimalFormat df = new DecimalFormat("###,###");
+        resultKRW = df.format(price) + "원";
+        
+        return resultKRW;
     }
 }
